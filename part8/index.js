72c9f7362b1e128d2e380d6ba547f6020c8142ff
Enrique Verea <kikeverea@gmail.com>
2023-01-30 00:51:18 +0100

    GraphQL backend: 8.1
    Implement bookCount and authorCount queries

--- part8/index.js
+++ part8/index.js
@@ -79,12 +79,30 @@ let books = [
 ]
 
 const typeDefs = gql`
-    type Query {
-    }
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]
+    id: ID!
+  }
+  
+  type Author {
+    name: String!
+    born: Int
+    id: ID!
+  }
+  
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+  }
 `
 
 const resolvers = {
   Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
 
